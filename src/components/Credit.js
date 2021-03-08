ae5b71299abce6dbe92c0f6d16099bdcda68c721
DonCastillo <don.qcastillo@yahoo.com>
2021-03-07 19:37:14 -0700

    added screenshots

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -5,10 +5,10 @@ const Credit = () => {
 	return (
 		<footer>
 			<a href="https://github.com/DonCastillo/" rel="noreferrer" target="_blank">
-				<i class="fab fa-github"></i>
+				<i className="fab fa-github"></i>
 			</a>
 			<a href="https://www.linkedin.com/in/don-castillo/" rel="noreferrer" target="_blank">
-				<i class="fab fa-linkedin"></i>
+				<i className="fab fa-linkedin"></i>
 			</a>
 		</footer>
 	);
